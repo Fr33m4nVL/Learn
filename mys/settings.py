aadb3859d3c92e721e5ad9d96ecbdf0a17157655
Fr33m4nVL <vladislavtrapezjikov@gmail.com>
2022-11-18 15:22:00 +0500

    Delete key

--- mys/settings.py
+++ mys/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cowr+n)dh5psr@6!o28t)m8!u6vsxojb$htq@(zxly!@^8^ezp'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
